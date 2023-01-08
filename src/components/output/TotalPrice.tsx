699f809f2a1a69b120fddc6492427614ac43cbb1
João Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2023-01-08 14:28:42 -0300

    dist

--- src/components/output/TotalPrice.tsx
+++
@@ -1,9 +0,0 @@
-import { TotalPriceType } from "../../types/Props";
-
-const TotalPrice = ({ price }: TotalPriceType) => {
-    return (
-        <span className='mt-16 span-price'>Preço: R$ {price}</span>
-    )
-}
-
-export default TotalPrice;
